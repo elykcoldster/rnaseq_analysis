a7ed543f12fba78e2935c0651542882134d23a31
Kyle Xiong <kxiong@andrew.cmu.edu>
2016-11-21 14:57:38 -0500

    Initial commit w/ tcga feature extraction (delta counts and tpm)

---
+++ driver.py
@@ -0,0 +1,4 @@
+import numpy as np
+import sklearn as sk
+from tcga_compile import extract_features
+
---
+++ tcga_compile.py
@@ -0,0 +1,17 @@
+import atpy
+import numpy as np
+
+def extract_features(tumor_file, normal_file):
+    tt = atpy.Table(tumor_file, type='ascii')
+    tn = atpy.Table(normal_file, type='ascii')
+    t_counts = tt.c
+    n_counts = tn.c
+    diff_counts = abs(t_counts - n_counts)
+    
+    # compile tpm data from atpy table -> normal and tumor table lengths should be equal
+    diff_tpm = []
+    for i in range(0,len(tt)):
+        diff_tpm.append(tt.data[i][0] - tn.data[i][0])
+
+    features = np.concatenate((diff_counts, diff_tpm), axis=1)
+    return features
