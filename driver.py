f491cc797bc7f0306fb28e1556dc931a6d16e3ed
Kyle Xiong <kylexiong1993@gmail.com>
2016-11-25 14:53:52 -0500

    Got rid of bug using normal files.

--- driver.py
+++ driver.py
@@ -6,8 +6,8 @@ from tcga_compile import extract_single_features
 
 # TODO: Be able to extract data from an entire brca/luad/blca/etc folder and cluster using kmeans
 #brca_features = extract_features("../TCGA_data/brca/TCGA-A7-A0CE/TCGA-A7-A0CE-01/tcgaquant/data.txt", "../TCGA_data/brca/TCGA-A7-A0CE/TCGA-A7-A0CE-11/tcgaquant/data.txt")
-directories = ['brca'] # for Windows test
-#directories = ['../TCGA_data/brca', '../TCGA_data/luad', '../TCGA_data/blca', '../TCGA_data/prad'] # for cluster unix use
+#directories = ['brca'] # for Windows test
+directories = ['../TCGA_data/brca', '../TCGA_data/luad', '../TCGA_data/blca', '../TCGA_data/prad'] # for cluster unix use
 
 features = []
 for i in range(0, len(directories)):
@@ -16,7 +16,6 @@ for i in range(0, len(directories)):
 		curr_path = directories[i] + '/' + subdirs[j]
 		normcancer = os.listdir(path=curr_path)
 		cancfiles = os.listdir(curr_path + '/' + normcancer[0] + '/tcgaquant')
-		normfiles = os.listdir(curr_path + '/' + normcancer[1] + '/tcgaquant')
 		features.append(extract_single_features(curr_path + '/' + normcancer[0] + '/tcgaquant/' + cancfiles[0]))
 
 kmeans = KMeans(n_clusters=len(directories)).fit(features)
