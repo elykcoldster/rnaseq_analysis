43a5f54b48886a728844cd8047427c3c84a2440d
Kyle Xiong <turkeymaniac@gmail.com>
2016-11-25 14:29:15 -0500

    Prototype of kmeans that sweeps through TCGA_data directory

--- driver.py
+++ driver.py
@@ -1,14 +1,22 @@
 import numpy as np
+import os
 from sklearn.cluster import KMeans
 from tcga_compile import extract_features
 
 # TODO: Be able to extract data from an entire brca/luad/blca/etc folder and cluster using kmeans
 #brca_features = extract_features("../TCGA_data/brca/TCGA-A7-A0CE/TCGA-A7-A0CE-01/tcgaquant/data.txt", "../TCGA_data/brca/TCGA-A7-A0CE/TCGA-A7-A0CE-11/tcgaquant/data.txt")
-brca_features = []
-brca_features.append(extract_features("tumor_sample.txt", "normal_sample.txt"))
-brca_features.append(extract_features("tumor_sample2.txt", "normal_sample2.txt"))
-brca_features.append(extract_features("tumor_sample2.txt", "normal_sample2.txt"))
-brca_features.append(extract_features("tumor_sample2.txt", "normal_sample2.txt"))
+# directories = ['brca', 'luad', 'blca', 'prad'] # for Windows test
+directories = ['../TCGA_data/brca', '../TCGA_data/luad', '../TCGA_data/blca', '../TCGA_data/prad'] # for cluster unix use
 
-kmeans = KMeans(n_clusters=2).fit(brca_features)
+features = []
+for i in range(0, len(directories)):
+	subdirs = os.listdir(path=directories[i])
+	for j in range(0, len(subdirs)):
+		curr_path = directories[i] + '/' + subdirs[j]
+		normcancer = os.listdir(path=curr_path)
+		cancfiles = os.listdir(curr_path + '/' + normcancer[0] + '/tcgaquant')
+		normfiles = os.listdir(curr_path + '/' + normcancer[1] + '/tcgaquant')
+		features.append(extract_features(curr_path + '/' + normcancer[0] + '/tcgaquant/' + cancfiles[0], curr_path + '/' + normcancer[1] + '/tcgaquant/' + normfiles[0]))
+
+kmeans = KMeans(n_clusters=len(directories)).fit(features)
 print(kmeans.labels_)
