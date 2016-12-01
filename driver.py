c27f4a874dc64fdf2c976ad3809aa5905e7a38bf
Kyle Xiong <kylexiong1993@gmail.com>
2016-12-01 16:51:05 -0500

    Added kmeans and pca for salmon. Some directories are missing salmon output. Need to write a check to see if salmon files exist.

--- driver.py
+++ driver.py
@@ -3,8 +3,8 @@ import os
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.externals import joblib
-from tcga_compile import extract_features
-from tcga_compile import extract_single_features
+from tcga_compile import extract_salmon_features
+from tcga_compile import extract_tcga_features
 
 # TODO: Be able to extract data from an entire brca/luad/blca/etc folder and cluster using kmeans
 #brca_features = extract_features("../TCGA_data/brca/TCGA-A7-A0CE/TCGA-A7-A0CE-01/tcgaquant/data.txt", "../TCGA_data/brca/TCGA-A7-A0CE/TCGA-A7-A0CE-11/tcgaquant/data.txt")
@@ -23,11 +23,19 @@ for i in range(0, len(directories)):
         tcgafiles = os.listdir(curr_path + '/' + normcancer[0] + '/tcgaquant')
         salmonfiles = os.listdir(curr_path + '/' + normcancer[0] + '/salmon')
         features.append(extract_tcga_features(curr_path + '/' + normcancer[0] + '/tcgaquant/' + tcgafiles[0]))
-        salmon_features.append(extract_salmon_features(curr_path + '/' + normcancer[0] + '/salmon/' + salmonfiles[0]))
+        salmon_features.append(extract_salmon_features(curr_path + '/' + normcancer[0] + '/salmon/quant.sf'))
 
-pca = PCA(n_components=24)
+ncomp = 24
+pca = PCA(n_components=ncomp)
 pca.fit_transform(features)
 kmeans = KMeans(n_clusters=len(directories)).fit(features)
 joblib.dump(kmeans, 'kmeans.pkl')
 joblib.dump(pca, 'pca.pkl')
 print(kmeans.labels_)
+
+spca = PCA(n_components=ncomp)
+spca.fit_transform(salmon_features)
+skmeans = KMeans(n_clusters=len(directories)).fit(salmon_features)
+joblib.dump(skmeans, 'skmeans.pkl')
+joblib.dump(pca, 'spca.pkl')
+print(skmeans.labels_)
